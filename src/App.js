75187b49b9684e4ad8315ebb4dd5833e840a1921
Fabrice Folituu <fabrice.folituu@lwane.com>
2021-09-08 20:08:54 -1000

    Create component Form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
 import './index.css';
 import Todo from "./components/Todo";
+import Form from './components/Form';
 
 function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      {/* Form Add Todo */}
-      <form>
-        <h2 className="label-wrapper">
-          <label htmlFor="new-todo-input" className="label__lg">
-            What needs to be done?
-          </label>
-        </h2>
-        <input
-          type="text"
-          id="new-todo-input"
-          className="input input__lg"
-          name="text"
-          autoComplete="off"
-        />
-        <button type="submit" className="btn btn__primary btn__lg">
-          Add
-        </button>
-      </form>
+      <Form />
       {/* Filters */}
       <div className="filters btn-group stack-exception">
         <button type="button" className="btn toggle-btn" aria-pressed="true">
