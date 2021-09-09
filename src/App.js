baaa2b2a67d7685900b7a6406b21cfbe86a9ddce
Fabrice Folituu <fabrice.folituu@lwane.com>
2021-09-08 20:50:34 -1000

    Create addTask function for New Task

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import './index.css';
 import Todo from "./components/Todo";
 import Form from './components/Form';
 import FilterButton from './components/FilterButton';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
 
 function App(props) {
+
+  const [tasks, setTasks] = useState(props.tasks);
+
+  function addTask(name) {
+    const newTask = { id: "todo" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
-      <Form />
+      <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
         <FilterButton name="all" pressed={true}/>
         <FilterButton name="active" pressed={false}/>
@@ -19,7 +29,7 @@ function App(props) {
       </h2>
       {/* Todo List */}
       <ul className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
-        {props.tasks.map(task =>
+        {tasks.map(task =>
           <Todo key={task.id} name={task.name} completed={task.completed} id={task.id} />
         )}
       </ul>
