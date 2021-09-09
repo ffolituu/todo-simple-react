50b5311759167dd1e00a50cd7b406ef7407b0b81
Fabrice Folituu <fabrice.folituu@lwane.com>
2021-09-08 20:18:33 -1000

    Create Component FilterButton

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import './index.css';
 import Todo from "./components/Todo";
 import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
 function App(props) {
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form />
-      {/* Filters */}
       <div className="filters btn-group stack-exception">
-        <button type="button" className="btn toggle-btn" aria-pressed="true">
-          <span className="visually-hidden">Show </span>
-          <span>all</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Active</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
-        <button type="button" className="btn toggle-btn" aria-pressed="false">
-          <span className="visually-hidden">Show </span>
-          <span>Completed</span>
-          <span className="visually-hidden"> tasks</span>
-        </button>
+        <FilterButton name="all" pressed={true}/>
+        <FilterButton name="active" pressed={false}/>
+        <FilterButton name="completed" pressed={false}/>
       </div>
       {/* Counter */}
       <h2 id="list-heading">
@@ -31,7 +19,7 @@ function App(props) {
       </h2>
       {/* Todo List */}
       <ul className="todo-list stack-large stack-exception" aria-labelledby="list-heading">
-        {props.tasks.map( task =>
+        {props.tasks.map(task =>
           <Todo key={task.id} name={task.name} completed={task.completed} id={task.id} />
         )}
       </ul>
